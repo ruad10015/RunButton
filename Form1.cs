ed379faf837f950bb8d7205332be8cc16aed671d
Dunya_sy83 <Dunya_sy83@itstep.edu.az>
2023-10-04 23:38:42 +0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int X = random.Next(0, 200);
+            int Y = random.Next(0, 200);
+
+            int randomR = random.Next(0, 255);
+            int randomG = random.Next(0, 255);
+            int randomB = random.Next(0, 255);
+
+            button1.Location = new Point(X, Y);
+        }
+    }
+}
